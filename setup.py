b56e22a26d8f9b5f134b8a2d415d5f79965af1a7
Scott <scott.linderman@gmail.com>
2015-06-23 09:53:45 -0400

    packaging for pypi

--- setup.py
+++ setup.py
@@ -5,12 +5,24 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(name='gslrandom',
-      version='0.1',
+      version='0.1.1',
       description='Cython wrappers for GSL random number generators',
-      author='Scott Linderman and Aaron Schein',
+      author='Scott Linderman, Aaron Schein, and Matthew Johnson',
       author_email='slinderman@seas.harvard.edu',
       url='http://www.github.com/slinderman/gslrandom',
+      license="MIT",
       packages=['gslrandom'],
       ext_modules=cythonize('**/*.pyx'),
       include_dirs=[np.get_include(),],
+      install_requires=[
+          'Cython >= 0.20.1',
+          'numpy'
+      ],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+          'Programming Language :: C++',
+      ],
+      keywords=['monte-carlo'],
+      platforms="ALL",
      )
